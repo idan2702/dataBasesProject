f86d9830dbf3a522d763d66ce81de982c350fab9
idan maslovski <idan2702@gmail.com>
2020-11-30 18:32:30 +0200

    register and login

---
+++ src/sample/Login.java
@@ -0,0 +1,46 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Login {
+    private Stage primaryStage;
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField pass;
+
+    public void set_stage(Stage _primaryStage){
+        this.primaryStage = _primaryStage;
+    }
+    @FXML
+    private void Login(ActionEvent event) throws Exception{
+        event.consume();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        Controller myController = loader.getController();
+        primaryStage.setTitle("Restaurants Guide");
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        myController.StartingMap(primaryStage);
+
+    }
+    @FXML
+    private void move_to_registration(ActionEvent event) throws Exception{
+        event.consume();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Register.fxml"));
+        Parent root = loader.load();
+        Register myController = loader.getController();
+        primaryStage.setTitle("Register");
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        myController.set_stage(primaryStage);
+    }
+}
